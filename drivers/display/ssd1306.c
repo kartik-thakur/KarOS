179658213eb35eb7b27fde6c3e2faf6dfa535dde
Kartik <kartik.thakur@hotmail.com>
2021-10-18 18:19:58 +0530

    display: ssd1306: add init sequence for ssd13106
    Add init sequence for ssd1306 display.
    
    Signed-off-by: Kartik <kartik.thakur@hotmail.com>

--- drivers/display/ssd1306.c
+++ drivers/display/ssd1306.c
@@ -5,9 +5,80 @@
 #include <karos/display/display-core.h>
 #include <karos/display/ssd1306.h>
 
+static int ssd1306_display_init(struct display_device *dev, uint32_t width,
+			 uint32_t height)
+{
+	struct ssd1306 *display;
+	struct i2c_dev *i2c_dev;
+	uint8_t com_pins;
+	uint8_t contrast;
+	uint8_t init_sequence[4][6] = {
+		{
+			SSD1306_DISPLAYOFF,
+			SSD1306_SETDISPLAYCLOCKDIV,
+			0x80,
+			SSD1306_SETMULTIPLEX
+		}, {
+			SSD1306_SETDISPLAYOFFSET,
+			0x0,
+			SSD1306_SETSTARTLINE | 0x0,
+			SSD1306_CHARGEPUMP
+		}, {
+			SSD1306_MEMORYMODE,
+			0x00,
+			SSD1306_SEGREMAP | 0x1,
+			SSD1306_COMSCANDEC
+		}, {
+			SSD1306_SETVCOMDETECT,
+			0x40,
+			SSD1306_DISPLAYALLON_RESUME,
+			SSD1306_NORMALDISPLAY,
+			SSD1306_DEACTIVATE_SCROLL,
+			SSD1306_DISPLAYON
+		},
+	};
+
+	if (width == 128 && height == 32) {
+		com_pins = 0x02;
+		contrast = 0x8F;
+	} else if (width == 128 && height == 64) {
+		com_pins = 0x12;
+		contrast = 0xCF;
+	} else if (width == 96 && height == 16) {
+		com_pins = 0x02;
+		contrast = 0xAF;
+	} else {
+		return -EINVAL;
+	}
+
+	display = (struct ssd1306*) dev->priv_data;
+	i2c_dev = display->i2c_dev;
+
+	i2c_dev_burst_write(i2c_dev, 0x0, init_sequence[0], 4);
+	i2c_dev_write(i2c_dev, 0x0, height - 1);
+
+	i2c_dev_burst_write(i2c_dev, 0x0, init_sequence[1], 4);
+	i2c_dev_write(i2c_dev, 0x0, 0x14);
+
+	i2c_dev_burst_write(i2c_dev, 0x0, init_sequence[2], 4);
+
+	i2c_dev_write(i2c_dev, 0x0, SSD1306_SETCOMPINS);
+	i2c_dev_write(i2c_dev, 0x0, com_pins);
+	i2c_dev_write(i2c_dev, 0x0, SSD1306_SETCONTRAST);
+	i2c_dev_write(i2c_dev, 0x0, contrast);
+
+	i2c_dev_write(i2c_dev, 0x0, SSD1306_SETPRECHARGE);
+	i2c_dev_write(i2c_dev, 0x0, 0xF1);
+
+	i2c_dev_burst_write(i2c_dev, 0x0, init_sequence[3], 6);
+
+	return 0;
+}
+
 int ssd1306_driver_probe(struct i2c_dev *dev)
 {
 	struct display_device *display_device;
+	struct ssd1306 *display;
 	int err = 0;
 
 	printf("SSD1306 Driver Probe\n");
@@ -16,9 +87,22 @@ int ssd1306_driver_probe(struct i2c_dev *dev)
 	if (!display_device)
 		return -ENOMEM;
 
+	display = calloc(1, sizeof(struct ssd1306));
+	if (!display) {
+		err = -ENOMEM;
+		goto out;
+	}
+
+	display->i2c_dev = dev;
+	display->display_device = display_device;
+	
 	display_device->name = "ssd1306";
-	display_device->priv_data = (void*) dev;
+	display_device->priv_data = (void*) display;
 	display_device_register(display_device);
+
+	ssd1306_display_init(display_device, 128, 32);
+
+out:
 	if (err)
 		free(display_device);
 
@@ -37,7 +121,7 @@ int ssd1306_driver_init(void)
 	if (!i2c_dev)
 		return -ENOMEM;
 
-	i2c_dev->bus = i2c1;
+	i2c_dev->bus = i2c0;
 	i2c_dev->addr = SSD1306_ADDR;
 	i2c_dev->ops = &ssd1306_i2c_dev_ops;
 
