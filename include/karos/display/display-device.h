7e248f77d8672ab881f6a64cfef00f73ab84ba29
Kartik <kartik.thakur@hotmail.com>
2021-10-18 22:48:44 +0530

    include: display: remove redundant ops
    Remove redundant ops from display-dev.h
    
    Signed-off-by: Kartik <kartik.thakur@hotmail.com>

--- include/karos/display/display-device.h
+++ include/karos/display/display-device.h
@@ -35,15 +35,4 @@ struct display_device {
 	bool				wrap;
 };
 
-void display_write_pixel(struct display_device *dev, int16_t x, int16_t y,
-			 uint16_t color);
-void display_set_rotation(struct display_device *dev, uint8_t rotation);
-void display_draw_bitmap(struct display_device *dev, int16_t x, int16_t y,
-			 uint8_t *bitmap, int16_t width, int16_t height,
-			 uint16_t color);
-void display_draw_char(struct display_device *dev, int16_t x, int16_t y,
-		       unsigned char c, uint16_t color, uint16_t bg_color,
-		       uint8_t size);
-void display_set_text_size(struct display_device *dev, uint8_t size);
-void display(struct display_device *dev);
 #endif
