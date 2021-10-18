17c39c5be34e00a9b60ab60290e8c59cc5e241fc
Kartik <kartik.thakur@hotmail.com>
2021-10-18 16:50:35 +0530

    drivers: add display driver registration support
    Add support for display driver registration.
    
    Signed-off-by: Kartik <kartik.thakur@hotmail.com>

--- include/karos/display/display-core.h
+++ include/karos/display/display-core.h
@@ -1,6 +1,9 @@
 #ifndef __DISPLAY_CORE_H__
 #define __DISPLAY_CORE_H__
 
+#include <karos/display/display-device.h>
+
+int display_device_register(struct display_device *dev);
 int display_drivers_init(void);
 
 #endif
--- include/karos/display/ssd1306.h
+++ include/karos/display/ssd1306.h
@@ -1,6 +1,15 @@
 #ifndef __SSD1306_H__
 #define __SSD1306_H__
 
+#include <karos/i2c/i2c-dev.h>
+#include <karos/i2c/i2c-core.h>
+
+#define SSD1306_ADDR		0x3C
+
+struct ssd1306 {
+	struct i2c_dev	*i2c_dev;
+};
+
 int ssd1306_driver_init(void);
 
 #endif
